4fc0f9092edda7a0509eee8304ee60e6c77a5606
Bharadwajasai <saibharadwaja610@gmail.com>
2017-07-09 14:57:06 +0530

    Update oddevenprogramme .java

--- oddevenprogramme .java
+++ oddevenprogramme .java
@@ -1,11 +1,3 @@
-Even or Odd:-
-
-int number=13;  
-    if(number%2==0){  
-        System.out.println("even number");  
-    }else{  
-        System.out.println("odd number"); 
-
 Positive and negative:-
 
 int number=-2;  
